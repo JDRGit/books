4ed5c6643d1ad00b77d1e6555d46556c434a6a4e
Jaime D. Rodriguez <118196254+JDRGit@users.noreply.github.com>
2023-03-08 15:02:10 -0500

    working state for bookshow, booklist, bookcreate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => book.id !== id);
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
-    console.log("Need to create a book with: ", title);
+    const updatedBooks = [...books, { id: Math.random(Math.random() * 9999), title, }];
+    setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className="app" >
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
