40b075dfc7efb3a016d105cd3b06b55b1723c475
Jaime D. Rodriguez <118196254+JDRGit@users.noreply.github.com>
2023-03-08 12:18:44 -0500

    working state for components / basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
-  return 
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("Need to create a book with: ", title);
+  };
+
+  return (
     <div>
-      <h1>Hello World</h1>
-    </div>;
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
