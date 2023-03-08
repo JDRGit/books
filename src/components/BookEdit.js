ec0d993c104d6f514154c68682d5bdf3f172d096
Jaime D. Rodriguez <118196254+JDRGit@users.noreply.github.com>
2023-03-08 15:24:14 -0500

    bookedit updated

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,27 @@
-function BookEdit() {
-    return <div>BookEdit</div>;
-}
+import { useState } from "react";
+
+const BookEdit = ({ book }) => {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // onCreate(title);
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="book-edit">
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button is-primary">Save</button>
+      </form>
+    </div>
+  );
+};
 
 export default BookEdit;
